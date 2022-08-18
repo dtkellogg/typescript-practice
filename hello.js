c4884f91986a5e30b4941a343d4c3240013a9adb
Toshi <dtkellogg10@gmail.com>
2022-08-18 10:16:52 -0700

    Doc's handbook - completed The Basics.

--- hello.js
+++ hello.js
@@ -1,12 +1,9 @@
 // -------------------- The following is from the Docs: -------------------- //
 // -------------------- The Basics (https://www.typescriptlang.org/docs/handbook/2/basic-types.html) -------------------- //
-
 console.log("Hello world!");
-
 // This is an industrial-grade general-purpose greeter function:
-function greet(person: string, date: Date) {
-  console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+function greet(person, date) {
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
 }
- 
 // greet("Brendan");
 greet("Toshi", new Date());
